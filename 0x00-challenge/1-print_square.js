f147ca6bfd2ba440a357759984d052b3f189c199
freakinlight <mazouznour0@gmail.com>
2024-05-30 11:30:29 -0700

    1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 /*
-    This script prints a square using the character '#'.
+    it prints a square with the character #
 
-    The size of the square must be provided as the first
-    argument to the program. If the argument is missing
-    or invalid, the program will display an error message
-    and usage instructions.
+    the squares size must be the first argument 
+    of the program.
 */
+
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
@@ -17,17 +16,9 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2], 10)
 
-if (isNaN(size)) {
-    process.stderr.write("Invalid argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
-}
-
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
 }
-
